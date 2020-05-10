d46bf68f327809b6b621b0a14aa40fa2eb215a8d
Mahesh Chary <imciiit@gmail.com>
2020-05-10 17:20:21 +0530

    lambda

---
+++ Python_Tcs_Modules/lambda/lambdaexample.py
@@ -0,0 +1,5 @@
+
+multiply_list_itself = list(map(lambda x : x**2 , [1,2,3,4]))
+sum_twolists = list(map(lambda x,y : x+y, [1,2,3,4,5],[5,4,3,2,1]))
+print(sum_twolists)
+print(multiply_list_itself)
