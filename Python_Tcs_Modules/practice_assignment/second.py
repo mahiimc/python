fe0f492037d86dcd44d5a58430838e14ae2fa8c8
Mahesh Chary <imciiit@gmail.com>
2020-05-10 14:10:30 +0530

    Second Solution

---
+++ Python_Tcs_Modules/practice_assignment/second.py
@@ -0,0 +1,11 @@
+"""2. Write a Python program to accept a number and check if its odd or even. Print appropriate messages.
+
+"""
+while(True):
+    num = int(input("Enter a Number :"))
+    
+    if (num %2 == 0):
+        print("Even")
+    else:
+        print("Odd")
+    
