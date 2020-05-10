3c1ce7a3826fc0c2d50e1e0b6eb60975e86ec4b7
Mahesh Chary <imciiit@gmail.com>
2020-05-10 11:34:23 +0530

    More functions added

--- Python_Tcs_Modules/Functions/functionExample.py
+++ Python_Tcs_Modules/Functions/functionExample.py
@@ -1,3 +1,4 @@
+
 def square(x):
     return x**2;
 
@@ -9,5 +10,32 @@ def greet(name, greetMsg="Good morning"):
     
 greet("mahesh");
 
-def greet2 (greetMsg="Good Morning !" ,name):   #SyntaxError: non-default argument follows default argument
-    print(name+" "+greetMsg)
+"""def greet2 (greetMsg="Good Morning !" ,name):   #SyntaxError: non-default argument follows default argument
+    print(name+" "+greetMsg)"""
+    
+def var_length_args (*args):
+    for arg in args:
+        print(arg , end=" ")
+
+var_length_args("mahesh","mahi","imc")
+
+def kwarg_function (**kwargs):
+    for key , value in kwargs.items():
+        print("%s == %s"%(key,value))
+        
+kwarg_function(first="Hello this is mahesh",second="mahesh")
+
+#args and kwargs together
+
+def var_together(*args , ** kwargs):
+    print(args)
+    print(kwargs)
+    
+    
+var_together("mahesh","mahi",first="mahesh", second="mahi")
+
+
+
+
+
+
