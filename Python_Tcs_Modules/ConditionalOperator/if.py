abe47bb5032ae34d2c4010e52add34a1ec648a4a
Mahesh Chary <imciiit@gmail.com>
2020-05-10 11:02:58 +0530

    Basics

---
+++ Python_Tcs_Modules/ConditionalOperator/if.py
@@ -0,0 +1,17 @@
+
+
+a=8
+if(isinstance(a,int)):
+    print("True")
+else :
+    print("False")
+    
+if (a >=9):
+    if ( a == 9):
+        print("a value",a)
+    else:
+        print("a value greater than 9")
+elif(a < 9):
+    print(a)
+else:
+    print(a)
---
+++ Python_Tcs_Modules/Variables/variables.py
@@ -0,0 +1,11 @@
+var1 = 10   # Number
+x=10.2
+x=20.567j
+print(isinstance(var1, int))
+print(isinstance(10.2,int))
+
+a=20
+b=6
+print("sum is",a+b)
+print("Division:",a/b)
+print("floor Division",a//b,"rounds to the floor value")
